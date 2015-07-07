b1521f520681a8b5c72f7b615aaa95e49a395223
Austin Chan <auscwork@gmail.com>
2015-07-07 01:34:34 -0700

    repo cleanup

---
+++ README.md
@@ -0,0 +1,21 @@
+# [HW Chronicle](http://hwchronicle.com/)
+
+HW Chronicle is the school newspaper of Harvard-Westlake High School. This website is the result of collaboration between myself and David Lim, the then-editor-in-chief of the newspaper. The site served to replace the unattractive and outdated newspaper website built by the school tech administrators on a DotNetNuke installation (which is just an extremely unfortunate name for a CMS framework) with a fresher completely redesigned installation of WordPress that would allow staff members to manage online content frustration-free.
+
+## Site Structure
+This site is structured as a WordPress installation with several custom post types that represent each type of article in order to provide clarity of separation for the different sections of the Chronicle staff. The project files in this repository are the contents of the WordPress theme that serves the website.
+
+## Awards
+
+The website was unexpectedly awarded the nation-wide Best High-School Newspaper Website Award by the NSPA, which is the most prestigious high-school journalism awards organization in the country. Staff celebrated all the awards HW Chronicle won that year with a huge cake and drinks. It was a good time. I believe the site also won awards from other organizations, but staff didn't really care about those other organizations so I guess they weren't that notable.
+
+## License
+
+HW Chronicle is [MPL licensed](./LICENSE?raw=true) and is available for limited public use under the license included in this repo.
+
+## Version Screenshots
+
+![ScreenShot](./screenshots/v1.png)
+![ScreenShot](./screenshots/v2.png)
+![ScreenShot](./screenshots/v3.png)
+![ScreenShot](./screenshots/v4.png)
--- header.php
+++ header.php
@@ -8,18 +8,16 @@
 <!-- the "no-js" class is for Modernizr. -->
 
 <!--
-  _   _             _____ _                     _      _      
- | | | |           / ____| |                   (_)    | |     
- | |_| |__   ___  | |    | |__  _ __ ___  _ __  _  ___| | ___ 
+  _   _             _____ _                     _      _
+ | | | |           / ____| |                   (_)    | |
+ | |_| |__   ___  | |    | |__  _ __ ___  _ __  _  ___| | ___
  | __| '_ \ / _ \ | |    | '_ \| '__/ _ \| '_ \| |/ __| |/ _ \
  | |_| | | |  __/ | |____| | | | | | (_) | | | | | (__| |  __/
   \__|_| |_|\___|  \_____|_| |_|_|  \___/|_| |_|_|\___|_|\___|
-                                                              
-                                                                  
-                                                                  
-Coded by Austin Chan '13.  
 
-[update] Wow this is the dirtiest code I've ever seen. Worst programming practice ever.  I'm ashamed of myself haha.
+
+
+Coded by Austin Chan '13.
 
 Almost completely from scratch, used a completely blank Reset wordpress theme.
 Brushed up my PHP for the confusing post setup, wrote A LOT of CSS, and a hint of JavaScript.
@@ -27,7 +25,7 @@ This site took FOREVER to code.  But I'm glad it's up :)
 
 Designed by David Lim and Austin Chan.
 We hope you like our site design, we spent A LOT of time on the design too, with about 5 other site design mockups.
- 
+
 After two and a half months and tens of thousands of lines of code, I'm so exhausted.
 
 Contact me at auscwork@gmail.com for any questions or anything at all.
@@ -38,12 +36,12 @@ Contact me at auscwork@gmail.com for any questions or anything at all.
 <head profile="http://gmpg.org/xfn/11">
 
 	<meta charset="<?php bloginfo('charset'); ?>">
-	
+
 	<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	
+
 	<?php if (is_search()) { ?>
-	<meta name="robots" content="noindex, nofollow" /> 
+	<meta name="robots" content="noindex, nofollow" />
 	<?php } ?>
 
 	<title>
@@ -66,7 +64,7 @@ Contact me at auscwork@gmail.com for any questions or anything at all.
 		         echo ' - page '. $paged; }
 		   ?>
 	</title>
-	
+
 	<meta name="title" content="<?php
 		      if (function_exists('is_tag') && is_tag()) {
 		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
@@ -92,17 +90,17 @@ Contact me at auscwork@gmail.com for any questions or anything at all.
 	<?php }else{ ?>
 	<meta name="description" content="<?php bloginfo('description'); ?>">
 	<?php } ?>
-	
+
 	<meta name="google-site-verification" content="">
 	<!-- Speaking of Google, don't forget to set your site up: http://google.com/webmasters -->
-	
+
 	<meta name="Copyright" content="Copyright Harvard-Westlake Chronicle 2012. All Rights Reserved.">
 
 	<!-- Dublin Core Metadata : http://dublincore.org/ -->
 	<meta name="DC.title" content="Harvard-Westlake Chronicle">
 	<meta name="DC.subject" content="The Online version of the monthly student newspaper of Harvard-Westlake.">
 	<meta name="DC.creator" content="By Austin Chan.">
-	
+
 	<?php if(is_single()){ global $post;?>
 	<meta property="og:title" content="<?php the_title(); ?>" />
 	<meta property="og:type" content="article" />
@@ -123,54 +121,54 @@ Contact me at auscwork@gmail.com for any questions or anything at all.
 	<meta property="og:description" content="The award-winning student newspaper of Harvard-Westlake"/>
 	<meta property="fb:app_id" content="249052195198188" />
 	<?php } ?>
-	
-	
+
+
 	<!--  Mobile Viewport meta tag
-	j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag 
+	j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag
 	device-width : Occupy full width of the screen in its current orientation
 	initial-scale = 1.0 retains dimensions instead of zooming out if page height > device height
 	maximum-scale = 1.0 retains dimensions instead of zooming in if page width < device width -->
 	<!-- Uncomment to use; use thoughtfully!
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 	-->
-	
+
 	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.png">
 	<!-- This is the traditional favicon.
 		 - size: 16x16 or 32x32
 		 - transparency is OK
 		 - see wikipedia for info on browser support: http://mky.be/favicon/ -->
-		 
+
 	<link rel="apple-touch-icon" href="<?php bloginfo('template_directory'); ?>/_/img/apple-touch-icon.png">
 	<!-- The is the icon for iOS's Web Clip.
 		 - size: 57x57 for older iPhones, 72x72 for iPads, 114x114 for iPhone4's retina display (IMHO, just go ahead and use the biggest one)
 		 - To prevent iOS from applying its styles to the icon name it thusly: apple-touch-icon-precomposed.png
 		 - Transparency is not recommended (iOS will put a black BG behind the icon) -->
-	
+
 	<!-- CSS: screen, mobile & print are all in the same file -->
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 	<!--[if IE]>
 	<style>
-	
+
 	html{height:100%;}
 	</style>
-	
+
 	<![endif]-->
 
 	<script src="<?php bloginfo('template_directory'); ?>/_/js/modernizr-1.7.min.js"></script>
-	
+
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
 	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 
 	<?php wp_head(); ?>
-	
-	
+
+
 <!-- 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/lightview/lightview.css"/> -->
 <link href='http://fonts.googleapis.com/css?family=Josefin+Slab' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/jquery.fancybox.css"/>
 <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/iconfont/css/font-awesome.min.css"/>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.easing.1.3.js"></script>	
-	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.livequery.js"></script>	
+	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.easing.1.3.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.livequery.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.flippy.min.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.fancybox.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.transit.min.js"></script>
